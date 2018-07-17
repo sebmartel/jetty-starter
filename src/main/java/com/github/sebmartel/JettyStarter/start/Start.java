1bb39d24d5f0162d42f3e97a24c3f3d386665f12
Seb <seb_martel@yahoo.com>
2018-07-16 20:26:33 -0700

    added exec:java support

--- src/main/java/com/github/sebmartel/JettyStarter/start/Start.java
+++ src/main/java/com/github/sebmartel/JettyStarter/start/Start.java
@@ -33,7 +33,7 @@ public class Start {
 			public void handle(String target, Request baseRequest,
 					HttpServletRequest request, HttpServletResponse response)
 					throws IOException, ServletException {
-
+				
 				response.setContentType("text/html;charset=utf-8");
 				response.setStatus(HttpServletResponse.SC_OK);
 				baseRequest.setHandled(true);
